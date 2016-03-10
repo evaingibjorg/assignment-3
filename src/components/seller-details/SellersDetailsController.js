d1e7cc93b04142f9e7a651bc571f0525190ceabe
evaingibjorg <evaingibjorg@gmail.com>
2016-03-10 15:29:15 +0000

    lagaði sellersdetailscontroller

--- src/components/seller-details/SellersDetailsController.js
+++ src/components/seller-details/SellersDetailsController.js
@@ -1,1 +1,5 @@
 "use strict";
+angular.module("project3App").controller("SellersDetailsController",
+function SellersController($scope, AppResource, $routeParams, $uibModal, $location, $translate) {
+	
+});
