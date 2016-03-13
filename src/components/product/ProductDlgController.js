dfa9e9866972a63592882767410c7e2010b21c40
Brndsh <bryndise14@hr.is>
2016-03-13 19:08:23 +0000

    add product verdur ad vera name price og mynd

--- src/components/product/ProductDlgController.js
+++ src/components/product/ProductDlgController.js
@@ -9,10 +9,10 @@ function ProductDlgController($scope, AppResource, $routeParams, $uibModal, moda
 
   $scope.onOk = function onOk(productInfo) {
       console.log(productInfo);
-      /*if(productInfo.name.length === 0 || productInfo.category.length === 0 || productInfo.imagePath.length === 0) {
+      if(productInfo.name.length === 0 || productInfo.price.length === 0 || productInfo.imagePath.length === 0) {
         centrisNotify.error("sellers.Messages.InputFail");
         return;
-      }*/
+      }
       $scope.$close(productInfo);
       
   };
